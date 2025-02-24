4e916a5aba27827f3643c6a8fced25d78cb47a1a
ESWAR YASWANTH KUMAR KODA <eswaryaswanthkumarkoda7@gmail.com>
2025-02-24 11:59:50 +0530

    Add files via upload

---
+++ SJF.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main(){
+    int n,p[20],bt[20],wt[20],tat[20];
+    printf("Enter the number of processes:");
+    scanf("%d",&n);
+    for(int i=1;i<n+1;i++){
+         p[i] = i;
+        printf("Enter the burst time for process %d--",i);
+        scanf("%d",&bt[i]);
+     }
+     for(int i=1;i<n;i++){
+          for(int j=1;j<n-i;j++){
+              if(bt[j] > bt[j+1]){
+                   int temp = bt[j];
+                   bt[j] = bt[j+1];
+                   bt[j+1] = temp;
+                   int temp1= p[j];
+                   p[j] = p[j+1];
+                   p[j+1] = temp1;
+               }
+           }
+      }
+      
+      printf("\nPROCESS\t\tBURST TIME\t\tTURNAROUND TIME\t\tWAITING TIME");
+      int ct = 0;
+      float avgwt = 0;
+      for(int i=1;i<n+1;i++){
+         ct = ct + bt[i];
+          tat[i ] = ct;
+          wt[i] = tat[i] - bt[i];
+          avgwt = avgwt + wt[i];
+          printf("\n%d                    %d                        %d                        %d",p[i],bt[i],tat[i],wt[i]);
+       }
+       printf("\nAverage Waiting Time: %f ",avgwt/n);
+       return 0;
+ }
