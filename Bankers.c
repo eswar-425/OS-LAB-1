755485232360158e89633e2bcb4a770d4220b2a4
ESWAR YASWANTH KUMAR KODA <eswaryaswanthkumarkoda7@gmail.com>
2025-03-17 11:52:38 +0530

    Bankers.c

--- Bankers.c
+++ Bankers.c
@@ -57,6 +57,7 @@ void main(){
         printf("\n");
     }
     int k = n;
+    int res = 0;
     while(k > 0){
     for(int i=0;i<n;i++){
         int count = 0;
@@ -66,9 +67,10 @@ void main(){
                 if(count == m && Finish[i] == false){
                     Finish[i] = true;
                     k -= 1;
-                    printf("p%d ",i+1);
+                    res += 1;
+                    printf("p%d ",i);
                     for(int j=0;j<m;j++){
-		     	work[j] += Allocation[i][j];
+		     work[j] += Allocation[i][j];
     		  }
                 }
             }
@@ -78,12 +80,41 @@ void main(){
         }
     }
     }
-    
+    if(res == n){
+      printf("\nSafe sequence is occured");
+      }
+      else{
+    	printf("\nDeadlock occured");
+      }
 }
 
 
+output:
 
 
-
+Number of processes: 5
+Number of resources type: 3
+Enter the Allocation 5*3 matrix:
+0 1 0
+2 0 0
+3 0 2
+2 1 1
+0 0 2
+Enter the Max 5*3 matrix:
+7 5 3 
+3 2 2
+9 0 2
+2 2 2
+4 3 3
+Enter the Available resources for first process:
+3 3 2
+Need matrix:
+7 4 3 
+1 2 2 
+6 0 0 
+0 1 1 
+4 3 1 
+p1 p3 p4 p0 p2 
+Safe sequence is occured
 
 
