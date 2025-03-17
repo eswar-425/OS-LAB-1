ba65fa5d952a39425698a3ae48cf5187021cc84b
ESWAR YASWANTH KUMAR KODA <eswaryaswanthkumarkoda7@gmail.com>
2025-03-17 11:19:28 +0530

    Bankers.c

--- Bankers.c
+++ Bankers.c
@@ -6,7 +6,7 @@ void main(){
     scanf("%d",&n);
     printf("Number of resources type: ");
     scanf("%d",&m);
-    int process[n],Allocation[n][m],Max[n][m],Available[n][m];
+    int process[n],Allocation[n][m],Max[n][m],Available[m];
     
     
     printf("Enter the Allocation %d*%d matrix:\n",n,m); //Allocation
@@ -15,7 +15,6 @@ void main(){
             scanf("%d",&Allocation[i][j]);
         }
     }
-    
     
     printf("Enter the Max %d*%d matrix:\n",n,m); //Max
     for(int i=0;i<n;i++){
@@ -26,19 +25,15 @@ void main(){
     
     
     printf("Enter the Available resources for first process:\n"); //Initialize Available
-    for(int i=0;i<1;i++){
         for(int j=0;j<m;j++){
-            scanf("%d",&Available[i][j]);
+            scanf("%d",&Available[j]);
         }
-    }
     
     
-    int work[n][m];
-    for(int i=0;i<1;i++){
+    int work[m];
         for(int j=0;j<m;j++){
-            work[i][j] = Available[i][j]; //Assign Availbale to work
+            work[j] = Available[j]; //Assign Availbale to work
         }
-    }
   
     
     
@@ -61,23 +56,27 @@ void main(){
         }
         printf("\n");
     }
-    
-    for(int i=0;i<1;i++){
+    int k = n;
+    while(k > 0){
+    for(int i=0;i<n;i++){
         int count = 0;
         for(int j=0;j<m;j++){
-            if(Need[i][j] <= work[i][j]){
+            if(Need[i][j] <= work[j]){
                 count++;
-                if(count == 3 && Finish[i] == false){
+                if(count == m && Finish[i] == false){
                     Finish[i] = true;
-                    for(int j=0;j<m;i++){
-								work[i][j] = work[i][j] + Allocation[i][j];
-    						}
+                    k -= 1;
+                    printf("p%d ",i+1);
+                    for(int j=0;j<m;j++){
+		     	work[j] += Allocation[i][j];
+    		  }
                 }
             }
             else{
                 break;
             }
         }
+    }
     }
     
 }
